4c2ca7e78780036e6ccc9a07c8ef12e420806ed1
Prapti Mahehswari <praptimaheshwari8@gmail.com>
2023-04-02 15:38:50 +0530

    initial commit

---
+++ src/main/java/com/hibernate/hibernateproject/HibernateprojectApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate.hibernateproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateprojectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/hibernate/hibernateproject/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.hibernate.hibernateproject.repository;
+
+import com.hibernate.hibernateproject.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Product findByName(String name);
+}
